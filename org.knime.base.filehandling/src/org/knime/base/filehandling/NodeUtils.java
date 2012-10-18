caafc4e9f6315b3eb433e2af76d46f29eb26a778
Patrick Winter <patrick.winter@uni-konstanz.de>
2012-10-18 07:50:42 +0000

    First implementation of remote copy node

---
+++ org.knime.base.filehandling/src/org/knime/base/filehandling/NodeUtils.java
@@ -0,0 +1,113 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2011
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME. The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ * 
+ * History
+ *   Oct 17, 2012 (Patrick Winter): created
+ */
+package org.knime.base.filehandling;
+
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.DataValue;
+import org.knime.core.node.InvalidSettingsException;
+
+/**
+ * Utility class for standard node implementation.
+ * 
+ * 
+ * @author Patrick Winter, University of Konstanz
+ */
+public final class NodeUtils {
+
+    private NodeUtils() {
+        // Disable default constructor
+    }
+
+    /**
+     * Checks if a column selection is set correctly. Will throw an exception if
+     * not.
+     * 
+     * 
+     * @param inSpec Specification of the input table
+     * @param selectionName Name of the column setting (will be used for the
+     *            error messages)
+     * @param selectedColumn Name of the selected column
+     * @param types Accepted column types, if none are given every type will be
+     *            accepted
+     * @throws InvalidSettingsException If the column is not set correctly
+     */
+    public static void checkColumnSelection(final DataTableSpec inSpec,
+            final String selectionName, final String selectedColumn,
+            final Class<? extends DataValue>... types)
+            throws InvalidSettingsException {
+        // Does the column setting reference to an existing column?
+        int columnIndex = inSpec.findColumnIndex(selectedColumn);
+        if (columnIndex < 0) {
+            throw new InvalidSettingsException(selectionName
+                    + " column not set");
+        }
+        // Check types if available
+        if (types.length > 0) {
+            // Is the column setting referencing to a column of a correct type?
+            DataType type = inSpec.getColumnSpec(columnIndex).getType();
+            boolean typeOk = false;
+            // Check each accepted type
+            for (int i = 0; i < types.length; i++) {
+                if (type.isCompatible(types[i])) {
+                    typeOk = true;
+                    break;
+                }
+            }
+            // Has a correct type been found?
+            if (!typeOk) {
+                throw new InvalidSettingsException(selectionName
+                        + " column not set");
+            }
+        }
+    }
+
+}
---
+++ org.knime.base.filehandling/src/org/knime/base/filehandling/remotecopy/RemoteCopyNodeModel.java
@@ -0,0 +1,211 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2011
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME. The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ *
+ * History
+ *   Sep 5, 2012 (Patrick Winter): created
+ */
+package org.knime.base.filehandling.remotecopy;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+import org.knime.base.filehandling.NodeUtils;
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.uri.URIDataValue;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+/**
+ * This is the model implementation of the node.
+ * 
+ * 
+ * @author Patrick Winter, University of Konstanz
+ */
+class RemoteCopyNodeModel extends NodeModel {
+
+    private SettingsModelString m_sourcecolumn;
+
+    private SettingsModelString m_targetcolumn;
+
+    /**
+     * Constructor for the node model.
+     */
+    protected RemoteCopyNodeModel() {
+        super(1, 0);
+        m_sourcecolumn = SettingsFactory.createSourceColumnSettings();
+        m_targetcolumn = SettingsFactory.createTargetColumnSettings();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+            final ExecutionContext exec) throws Exception {
+        int sourceIndex =
+                inData[0].getDataTableSpec().findColumnIndex(
+                        m_sourcecolumn.getStringValue());
+        int targetIndex =
+                inData[0].getDataTableSpec().findColumnIndex(
+                        m_targetcolumn.getStringValue());
+        for (DataRow row : inData[0]) {
+            DataCell sourceCell = row.getCell(sourceIndex);
+            DataCell targetCell = row.getCell(targetIndex);
+            if (!sourceCell.isMissing() && !targetCell.isMissing()) {
+                URI sourceURI =
+                        ((URIDataValue)row.getCell(sourceIndex))
+                                .getURIContent().getURI();
+                URI targetURI =
+                        ((URIDataValue)row.getCell(targetIndex))
+                                .getURIContent().getURI();
+                Copier.copy(sourceURI, targetURI, exec);
+                // TODO update progress
+            }
+        }
+        return new BufferedDataTable[]{};
+    }
+
+    /**
+     * Check if the settings are all valid.
+     * 
+     * 
+     * @param inSpec Specification of the input table
+     * @throws InvalidSettingsException If the settings are incorrect
+     */
+    @SuppressWarnings("unchecked")
+    private void checkSettings(final DataTableSpec inSpec)
+            throws InvalidSettingsException {
+        String source = m_sourcecolumn.getStringValue();
+        String target = m_targetcolumn.getStringValue();
+        NodeUtils.checkColumnSelection(inSpec, "Source", source,
+                URIDataValue.class);
+        NodeUtils.checkColumnSelection(inSpec, "Target", target,
+                URIDataValue.class);
+        if (source.equals(target)) {
+            throw new InvalidSettingsException(
+                    "Source and target do not differ");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void reset() {
+        // Not used
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+            throws InvalidSettingsException {
+        checkSettings(inSpecs[0]);
+        return new DataTableSpec[]{};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        m_sourcecolumn.saveSettingsTo(settings);
+        m_targetcolumn.saveSettingsTo(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        m_sourcecolumn.loadSettingsFrom(settings);
+        m_targetcolumn.loadSettingsFrom(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        m_sourcecolumn.validateSettings(settings);
+        m_targetcolumn.validateSettings(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadInternals(final File internDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+        // Not used
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveInternals(final File internDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+        // Not used
+    }
+
+}
