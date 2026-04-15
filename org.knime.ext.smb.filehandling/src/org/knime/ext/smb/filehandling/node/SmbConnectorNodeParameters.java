e25b15b8878dc69f368f341f43b9d12aeb083fc7
Paul Baernreuther <paul.baernreuther@knime.com>
2026-04-15 13:02:01 +0200

    UIEXT-3553: Moved getCredentialsProvider to public interface
    UIEXT-3553 (Move getCredentialsProvider to the public NodeParametersInput interface)

--- org.knime.ext.smb.filehandling/src/org/knime/ext/smb/filehandling/node/SmbConnectorNodeParameters.java
+++ org.knime.ext.smb.filehandling/src/org/knime/ext/smb/filehandling/node/SmbConnectorNodeParameters.java
@@ -55,7 +55,6 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.workflow.CredentialsProvider;
-import org.knime.core.webui.node.dialog.defaultdialog.impl.NodeParametersInputImpl;
 import org.knime.node.parameters.widget.file.customfilesystem.FSConnectionProvider;
 import org.knime.node.parameters.widget.file.FileSelectionWidget;
 import org.knime.node.parameters.widget.file.SingleFileSelectionMode;
@@ -398,7 +397,7 @@ final class SmbConnectorNodeParameters implements NodeParameters {
 
         private static CredentialsProvider getCredentialsProvider(
                 final NodeParametersInput input) {
-            return ((NodeParametersInputImpl) input).getCredentialsProvider().orElseThrow();
+            return input.getCredentialsProvider().orElseThrow();
         }
     }
 
